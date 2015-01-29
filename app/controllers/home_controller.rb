c97289899dcca54125cb23a5be689440335e6839
Erika Mesh <esm1884@vm-006.casci.rit.edu>
2015-01-29 11:26:53 -0500

    Issue #1 Made some initial changes to use RIT/SciSPIF specific text and logos. Also reworked the front page a bit to use our research workflow and link to a "goals" sample question.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
   skip_before_filter :initialize_session, :set_session_timestamp, :record_action, :view_filter, :set_pairwise_credentials, :set_locale, :set_p3p_header, :only => [:cookies_blocked]
 
   def index
-    @example_earl = 'planyc_example'
+    @example_earl = 'goals'
     Question.timeout = 0.5
     begin
       @stats = Question.get(:site_stats)
