bc26f6caf3bb0dd2ecce3fced2f0eb252217d915
Erika S. Mesh <erika.s.mesh@gmail.com>
2015-04-13 17:53:55 -0400

    Store sessions in the DB instead of the browser

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140828214907) do
+ActiveRecord::Schema.define(:version => 20150413192405) do
 
   create_table "ages", :force => true do |t|
     t.datetime "created_at"
@@ -181,6 +181,16 @@ ActiveRecord::Schema.define(:version => 20140828214907) do
   end
 
   add_index "session_infos", ["session_id"], :name => "index_session_infos_on_session_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "slugs", :force => true do |t|
     t.string   "name"
